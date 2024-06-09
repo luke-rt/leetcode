90b7fbeda87de430394e6031f69abb3458a7fc3e
luke-rt <luke.rtong@gmail.com>
2024-06-09 16:30:06 -0400

    p974

---
+++ java/P974.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int[] freq = new int[k];
+        freq[0] = 1; // old: count += freq[0]; same math as just adding another instance
+        int count = 0;
+
+        int tmp = 0;
+        for(int i = 0; i < nums.length; ++i) {
+            tmp = (tmp + nums[i]) % k;
+            if(tmp < 0) tmp += k;
+
+            // initial freq: 3, adding new instance means 3 new ordered pairs of values
+            count += freq[tmp]; 
+            ++freq[tmp];
+        }
+
+        return count;
+    }
+}
+
